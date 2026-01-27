bd52e8928886d49b882ac432b3fba71fe40af04b
Mitja Kurath <mitja.kurath@kyberna.com>
2026-01-27 09:26:52 +0100

    feat: initial commit

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'payload-oauth2/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'payload-oauth2/rsc'
+
+export const importMap = {
+  'payload-oauth2/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'payload-oauth2/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
